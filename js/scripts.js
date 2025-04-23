3ffa11e8916e3d17d5c6e0c8893767a7221703d0
TouyeeL <156361293+TouyeeL@users.noreply.github.com>
2025-04-23 03:13:53 +0000

    updated typeface edited js

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,78 @@
 // Scripting
 
 // Data source
-const url = "https://assets.codepen.io/16425/web-3-spring-2024-roster.json";
+// https://assets.codepen.io/16425/Spring-2025-Roster.json
 
-// Get data
-fetch(url)
-  .then( response  => response.json())
-  .then( data  => {
+const dataSource = 'https://assets.codepen.io/16425/Spring-2025-Roster.json';
+
+// GET elements
+const container = document.querySelector('.card');
+
+// fetch
+fetch( dataSource )
+.then( res => res.json() )
+.then( students => {
+  
+  // checkcheck: Data ok? 
+  // console.log( students[3].name );
+  
+  students.forEach( student => {
     
-    // check-check: is the data good?
-    console.log(data);
-    console.log(data.Name);
-    console.log(data.Image);
-
-    // get container for data
-    const roster = document.querySelector(".roster");
-
-    // loop through data
-    data.forEach( student => {
+  // template
+  const template = `
+    <figure style=
+    "border-color: ${student.favoriteColor};
+    background: linear-gradient(white, ${student.favoriteColor})
+    ">
+        <img src="${student.imageUrl}" />
       
-      // template
-      const template = `
-          <figure>
-            <figcaption> ${student.Name} </figcaption>
-            <img src=" ${student.Image} " alt=" ${student.Name} ">
-          </figure>
-       `;
+        <h2>
+          ${student.name}
+          ${student.status}
+        </h2>
+        
+        <p>
+         Fun Fact: ${student.funFact}
+        </p>
+        
+        <p>
+        Program: ${student.team}
+        </p>
+        
+        <p>
+        Quote: ${student.motto}
+        </p>
+     
+        <p>
+        Favorite Character: ${student.favoriteSimpsonsCharacter}
+        </p>
+        
+        <p>
+        Favorite Artist: ${student.favoriteBand}
+        </p>
+        
+        <p>
+        Favorite Food: ${student.favoriteFood}
+        </p>
+        
+        <p>
+        Super Power: ${student.talent}
+        </p>
+        
+        <em> 
+         Favorite Song: ${student.favoriteSong}
+        </em>
+        
+        
+    </g>
+    `;
+  
+  // SET it. rendering
+  container.insertAdjacentHTML( 'afterbegin', template);
 
-      // insert EACH `student` record into container
-      roster.insertAdjacentHTML("afterbegin", template);
-    });
-  });
+    
+  });  
+  
+  
+});
+
